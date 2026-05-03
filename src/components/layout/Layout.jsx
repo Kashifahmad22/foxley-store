9132d267d61d3032d7f1af3bb70f1368e8432a61
Kashif <mkkashif2002@gmail.com>
2026-05-03 18:35:45 +0530

    Refactor app to remove Base44 dependencies and stabilize core storefront routes

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,15 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
-import Footer from './Footer';
 
 export default function Layout() {
   return (
-    <div className="min-h-screen flex flex-col bg-foxley-bone font-jakarta">
+    <div className="min-h-screen bg-foxley-bone">
       <Navbar />
-      <main className="flex-1 pt-[72px]">
-        <Outlet />
-      </main>
-      <Footer />
+      <main><Outlet /></main>
     </div>
   );
 }
